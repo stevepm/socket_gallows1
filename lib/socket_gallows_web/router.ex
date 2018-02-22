ba66bbb3c6b60f96607d1727ae095dac18f8c07d
Steven Magelowitz <steven.magelowitz@gmail.com>
2018-02-21 21:35:29 -0800

    initial commit

---
+++ lib/socket_gallows_web/router.ex
@@ -0,0 +1,27 @@
+defmodule SocketGallowsWeb.Router do
+  use SocketGallowsWeb, :router
+
+  pipeline :browser do
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+  end
+
+  pipeline :api do
+    plug(:accepts, ["json"])
+  end
+
+  scope "/", SocketGallowsWeb do
+    # Use the default browser stack
+    pipe_through(:browser)
+
+    get("/", PageController, :index)
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", SocketGallowsWeb do
+  #   pipe_through :api
+  # end
+end
